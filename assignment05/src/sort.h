3b8f3cabdf62dee9d17f569f93ed1e674fdfd390
SeanA <sean.ammon@mymail.champlain.edu>
2025-10-02 13:48:27 -0400

    Implemented Sorting Algorithms for Assignment 5

--- assignment05/src/sort.h
+++ assignment05/src/sort.h
@@ -38,67 +38,124 @@
 
 using namespace std;
 
-namespace csi281 {
+namespace csi281
+{
 
-  // Performs an in-place ascending sort of *array*
-  // using the merge sort algorithm
-  // *start* is the first element of the array to start sorting from
-  // *end* is the end element for the elements to be sorted
-  // *end* will be the length of the array - 1 for a first run
-  // NOTE: Your solution MUST use std::inplace_merge
-  // http://www.cplusplus.com/reference/algorithm/inplace_merge/
-  template <typename T> void mergeSort(T array[], const int start, const int end) {
-    // YOUR CODE HERE
-  }
+    // Performs an in-place ascending sort of *array*
+    // using the merge sort algorithm
+    // *start* is the first element of the array to start sorting from
+    // *end* is the end element for the elements to be sorted
+    // *end* will be the length of the array - 1 for a first run
+    // NOTE: Your solution MUST use std::inplace_merge
+    // http://www.cplusplus.com/reference/algorithm/inplace_merge/
+    template <typename T> void mergeSort(T array[], const int start, const int end)
+    { 
+        if (start >= end) return;
 
-  // setup random number generator
-  static random_device rd;
-  static mt19937 rng(rd());
-  // you can use uniform_int_distribution and rng to create
-  // a random int in a certain range
+        int mid = start + (end - start) / 2;
+        mergeSort(array, start, mid);
+        mergeSort(array, mid + 1, end);
 
-  // Performs an in-place ascending sort of *array*
-  // using the quicksort algorithm
-  // *start* is the first element of the array to start sorting from
-  // *end* is the end element for the elements to be sorted
-  // *end* will be the length of the array - 1 for a first run
-  // NOTE: Your solution should use std::swap
-  // http://www.cplusplus.com/reference/algorithm/swap/
-  // NOTE: Your solution MUST use a random pivot
-  // TIP: It may be helpful to swap the pivot to the end,
-  // sort the center of the range, and then move the pivot back to
-  // the appropriate place
-  template <typename T> void quickSort(T array[], const int start, const int end) {
-    // YOUR CODE HERE
-  }
+        std::inplace_merge(array + start, array + mid + 1, array + end + 1);
 
-  // Performs an in-place ascending sort of *array*
-  // using the insertion sort algorithm
-  // *start* is the first element of the array to start sorting from
-  // *end* is the end element for the elements to be sorted
-  // *end* will be the length of the array - 1 for a first run
-  // NOTE: You can modify your old implementation from assignment 4
-  // OR if you believe yours was incorrect, email me and I will
-  // send you mine assuming you are done with assignment 4,
-  // but you will still need to modify it for this assignment
-  // as described below
-  // NOTE: You will need to modify the implementation to only
-  // sort part of the array as per the parameters of this version
-  template <typename T> void insertionSort(T array[], const int start, const int end) {
-    // YOUR CODE HERE
-  }
+    }
 
-  // Performs an in-place ascending sort of *array*
-  // using merge sort until there is less than 10 elements, and then
-  // the insertion sort algorithm
-  // *start* is the first element of the array to start sorting from
-  // *end* is the end element for the elements to be sorted
-  // *end* will be the length of the array - 1 for a first run
-  // TIP: You can copy your implementation of merge sort in here, and
-  // should be able to call the insertionSort above
-  template <typename T> void hybridSort(T array[], const int start, const int end) {
-    // YOUR CODE HERE
-  }
+    // setup random number generator
+    static random_device rd;
+    static mt19937 rng(rd());
+    // you can use uniform_int_distribution and rng to create
+    // a random int in a certain range
+
+    // Performs an in-place ascending sort of *array*
+    // using the quicksort algorithm
+    // *start* is the first element of the array to start sorting from
+    // *end* is the end element for the elements to be sorted
+    // *end* will be the length of the array - 1 for a first run
+    // NOTE: Your solution should use std::swap
+    // http://www.cplusplus.com/reference/algorithm/swap/
+    // NOTE: Your solution MUST use a random pivot
+    // TIP: It may be helpful to swap the pivot to the end,
+    // sort the center of the range, and then move the pivot back to
+    // the appropriate place
+    template <typename T> void quickSort(T array[], const int start, const int end)
+    {
+        // YOUR CODE HERE
+        if (start >= end) return;
+
+        uniform_int_distribution<int> distrubition(start, end);
+        int pivot = distrubition(rng);
+        int lowerIndex = start;
+
+        // Swapping pivot to the 0th position
+        if (pivot != start)
+        {
+            std::swap(array[pivot], array[start]);
+            pivot = start;
+        }
+
+        for (int i = start + 1; i < end + 1; i++)
+        {
+            if (array[i] < array[pivot])
+            {
+                lowerIndex++;
+                std::swap(array[i], array[lowerIndex]);
+            }
+        }
+        
+        // Swapping the pivot to the middle
+        std::swap(array[pivot], array[lowerIndex]);
+        pivot = lowerIndex;
+        quickSort(array, start, lowerIndex - 1);
+        quickSort(array, lowerIndex + 1, end);
+    }
+
+    // Performs an in-place ascending sort of *array*
+    // using the insertion sort algorithm
+    // *start* is the first element of the array to start sorting from
+    // *end* is the end element for the elements to be sorted
+    // *end* will be the length of the array - 1 for a first run
+    // NOTE: You can modify your old implementation from assignment 4
+    // OR if you believe yours was incorrect, email me and I will
+    // send you mine assuming you are done with assignment 4,
+    // but you will still need to modify it for this assignment
+    // as described below
+    // NOTE: You will need to modify the implementation to only
+    // sort part of the array as per the parameters of this version
+    template <typename T> void insertionSort(T array[], const int start, const int end)
+    {
+        if (start >= end) return;
+
+        for (int i = start + 1; i < (end + 1); i++)
+        {
+            T currentValue = array[i];
+            int checkIndex = i - 1;
+
+            while (checkIndex >= 0 && array[checkIndex] > currentValue)
+            {
+                array[checkIndex + 1] = array[checkIndex];
+                checkIndex--;
+            }
+            array[checkIndex + 1] = currentValue;
+        }
+    }
+
+    // Performs an in-place ascending sort of *array*
+    // using merge sort until there is less than 10 elements, and then
+    // the insertion sort algorithm
+    // *start* is the first element of the array to start sorting from
+    // *end* is the end element for the elements to be sorted
+    // *end* will be the length of the array - 1 for a first run
+    // TIP: You can copy your implementation of merge sort in here, and
+    // should be able to call the insertionSort above
+    template <typename T> void hybridSort(T array[], const int start, const int end)
+    {
+        if (start >= end) return;
+
+        if (end - start + 1 < 10)
+            insertionSort(array, start, end);
+        else
+            mergeSort(array, start, end);
+    }
 
 }  // namespace csi281
 
